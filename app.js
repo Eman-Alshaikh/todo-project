2b5898c997d621b40c6081936fd56fed143e686a
Eman Al-Shaikh Hussain <engemanalsheikh@gmail.com>
2022-01-23 19:18:39 +0200

    task 6

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 'use strict';
- 
+const person = [];
  
 alert('This is alert message');
  
 var  inp = prompt('please Enter your name here : ');
- 
+person[0]= console.log(inp);
  
 var  gender = prompt('please Enter your gender here : you must enter "male" or "female"');
  
- 
+person[1]= console.log(gender);
  
  
 var  age = prompt('please Enter your age here : ');
 console.log('Your age is : ',age);
+person[2]= console.log(age);
     if (age<=0)
     {
         alert('the age must be bigger than 0 ');  
@@ -37,3 +38,18 @@ else
      
 }
 
+
+let proceed = confirm("do you want to answer another 3 questions?");
+if (proceed) {
+    var  lastName = prompt('please Enter your last name here : ');
+     person[3]= console.log(lastName);
+
+     var  luckyNum= prompt('please Enter your lucky digit number : ');
+     person[4]= console.log(luckyNum);
+
+     var  birthday = prompt('please Enter your birthday   : ');
+     person[5]= console.log(birthday);
+
+} else {
+  
+}
